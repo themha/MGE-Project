bdc0990952bccc1f64a2a240995ff95ab06a507e
Marco Martinez <mmmartinez@bluewin.ch>
2020-10-15 07:39:38 +0200

    add login page #4

---
+++ app/src/main/java/com/ost/rj/mge/testat/view/LoginPage.kt
@@ -0,0 +1,12 @@
+package com.ost.rj.mge.testat.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ost.rj.mge.testat.R
+
+class LoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+    }
+}
