960bd7033d11f0640fc5ba8bce33fcb99a515e5a
Marco Martinez <mmmartinez@bluewin.ch>
2020-10-15 09:23:35 +0200

    add login Page #4

--- app/src/main/java/com/ost/rj/mge/testat/view/LoginPage.kt
+++ app/src/main/java/com/ost/rj/mge/testat/view/LoginPage.kt
@@ -1,12 +1,32 @@
 package com.ost.rj.mge.testat.view
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.ost.rj.mge.testat.R
+import com.ost.rj.mge.testat.activities.FeedActivity
 
 class LoginPage : AppCompatActivity() {
+
+    companion object {
+
+        fun createIntent(context: Context) : Intent {
+            val intent : Intent = Intent(context, LoginPage::class.java)
+            return intent
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+        val feedButton : Button = findViewById(R.id.feedbutton)
+        feedButton.setOnClickListener {
+            val feedActivityIntent : Intent = FeedActivity.createIntent(this)
+            startActivity(feedActivityIntent)
+        }
     }
+
+
 }
